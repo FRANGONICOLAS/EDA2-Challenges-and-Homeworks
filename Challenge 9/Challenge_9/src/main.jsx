5e8b7a5d0f4284f6a20dd39300b842faf84e436f
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-09-25 16:41:33 -0500

    CHALLENGE9

--- Challenge 9/Challenge_9/src/main.jsx
+++ Challenge 9/Challenge_9/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
-import { Father } from './components/Father.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Father/>
+    <App />
   </StrictMode>,
 )
