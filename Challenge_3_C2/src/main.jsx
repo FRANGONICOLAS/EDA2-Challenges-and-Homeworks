b78e2f09e9f6730e74fc0b5990d08a0e2ad51d6b
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-10-08 23:11:05 -0500

    Challenge_3 corte 2

--- Challenge_3_C2/src/main.jsx
+++ Challenge_3_C2/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import FirstApp from './FirstApp.jsx'
+import GifExpertApp from './ComponentApp'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FirstApp />
+    <GifExpertApp />
   </StrictMode>,
 )
