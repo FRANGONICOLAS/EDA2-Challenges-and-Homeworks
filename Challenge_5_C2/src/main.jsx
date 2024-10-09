1bc5f714beb26c4127a19cf30e7e7cb490e3e0b7
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-10-08 23:49:19 -0500

    Challenge 5 corte 2

--- Challenge_5_C2/src/main.jsx
+++ Challenge_5_C2/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GifGrid } from './Components/GifGrid'
-import { GifItem } from './Components/GiftItem'
+import MyComponent from './Components/myComponent'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GifGrid />
-    <GifItem />
+    <MyComponent/>
   </StrictMode>,
 )
